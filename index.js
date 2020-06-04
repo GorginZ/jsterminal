420a511df04ef0ca41bf12d5bae775f0528c4564
GorginZ <georgia.z.leng@gmail.com>
2020-06-04 17:18:24 +1000

    added dice roll for character creation

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const readlineSync = require('readline-sync');
 
 console.log("Hi there, welcome to the counsel")
-setTimeout(function() {
+
     console.log("There is great wisdom here")
-}, 3000);
+
+
+
+race = ['High Elf', 'Human', 'Dragonborn', 'Dwarf', 'Gnome', 'Orc', 'Half-Orc', 'Half-Elf', 'Tiefling'],
+index = readlineSync.keyInSelect(race, 'Which race?');
+console.log('Ok, ' + race[index]);
+
 let name = ""
-setTimeout(function() {
-    name = readlineSync.question('What is your name? ');
-
-}, 6000);
+name = readlineSync.question('What is your name? ');
 
 
-setTimeout(function() {
+classes = ['Paladin', 'Bard', 'Barbarian', 'Ranger', 'Cleric','Druid','Fighter','Monk','Wizard','Warlock','Sorcerer','Rogue'],
+class_index = readlineSync.keyInSelect(classes, `What class are you ${race[index]}?`);
+
+
+alignment = ['Lawful Good', 'Chaotic Good', 'Chaotic Neutral', 'Lawful Neutral', 'Chaotic Evil', 'Lawful Evil'],
+alignment_index = readlineSync.keyInSelect(alignment, `What is your alignment?`);
+console.log('Ok, ' + alignment[alignment_index] + ' is your alignment.');
+
+// dice roll for stats
+function rolldice() {
+    var x = Math.floor(Math.random() * 10 + 1);
+    var dicetotal = x;
+    console.log(x);
+};
+
+rolldice()
+
     console.log(name + " I can see you're troubled.")
-}, 8000);
-
-
-
-
